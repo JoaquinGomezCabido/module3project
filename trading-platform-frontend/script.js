edb038b03b3931cb834875b5412bee90ce2a8b43
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-10-01 14:48:50 +0100

    added current date

--- trading-platform-frontend/script.js
+++ trading-platform-frontend/script.js
@@ -37,6 +37,68 @@ const h1 = document.querySelector("h1");
 const tableBody = document.querySelector("tbody");
 const chartContainer = document.querySelector("#chart-container");
 const tradesLogContainer = document.querySelector("#trades-log-container");
+const availableDates = [
+	"January 2020",
+	"February 2020",
+	"March 2020",
+	"April 2020",
+	"May 2020",
+	"June 2020",
+	"July 2020",
+	"August 2020",
+	"September 2020",
+	"October 2020",
+	"November 2020",
+	"December 2020",
+	"January 2021",
+	"February 2021",
+	"March 2021",
+	"April 2021",
+	"May 2021",
+	"June 2021",
+	"July 2021",
+	"August 2021",
+	"September 2021",
+	"October 2021",
+	"November 2021",
+	"December 2021",
+	"January 2022",
+	"February 2022",
+	"March 2022",
+	"April 2022",
+	"May 2022",
+	"June 2022",
+	"July 2022",
+	"August 2022",
+	"September 2022",
+	"October 2022",
+	"November 2022",
+	"December 2022",
+	"January 2023",
+	"February 2023",
+	"March 2023",
+	"April 2023",
+	"May 2023",
+	"June 2023",
+	"July 2023",
+	"August 2023",
+	"September 2023",
+	"October 2023",
+	"November 2023",
+	"December 2023",
+	"January 2024",
+	"February 2024",
+	"March 2024",
+	"April 2024",
+	"May 2024",
+	"June 2024",
+	"July 2024",
+	"August 2024",
+	"September 2024",
+	"October 2024",
+	"November 2024",
+	"December 2024",
+];
 
 // CREATE NEW GAME
 
@@ -168,8 +230,10 @@ function playGame(username, company) {
 
 	let currentPrice = 100;
 	let startData = new Array(60).fill(100);
+	let currentDate = "January 2020";
 	let buyPrice;
 	let sellPrice;
+	let annotations = { points: [] };
 
 	let buyPricesList = [];
 	let sellPricesList = [];
@@ -220,85 +284,13 @@ function playGame(username, company) {
 			},
 		},
 		xaxis: {
-			categories: [
-				"January 2020",
-				"February 2020",
-				"March 2020",
-				"April 2020",
-				"May 2020",
-				"June 2020",
-				"July 2020",
-				"August 2020",
-				"September 2020",
-				"October 2020",
-				"November 2020",
-				"December 2020",
-				"January 2021",
-				"February 2021",
-				"March 2021",
-				"April 2021",
-				"May 2021",
-				"June 2021",
-				"July 2021",
-				"August 2021",
-				"September 2021",
-				"October 2021",
-				"November 2021",
-				"December 2021",
-				"January 2022",
-				"February 2022",
-				"March 2022",
-				"April 2022",
-				"May 2022",
-				"June 2022",
-				"July 2022",
-				"August 2022",
-				"September 2022",
-				"October 2022",
-				"November 2022",
-				"December 2022",
-				"January 2023",
-				"February 2023",
-				"March 2023",
-				"April 2023",
-				"May 2023",
-				"June 2023",
-				"July 2023",
-				"August 2023",
-				"September 2023",
-				"October 2023",
-				"November 2023",
-				"December 2023",
-				"January 2024",
-				"February 2024",
-				"March 2024",
-				"April 2024",
-				"May 2024",
-				"June 2024",
-				"July 2024",
-				"August 2024",
-				"September 2024",
-				"October 2024",
-				"November 2024",
-				"December 2024",
-				"January 2025",
-				"February 2025",
-				"March 2025",
-				"April 2025",
-				"May 2025",
-				"June 2025",
-				"July 2025",
-				"August 2025",
-				"September 2025",
-				"October 2025",
-				"November 2025",
-				"December 2025",
-			],
+			categories: availableDates,
 		},
 		yaxis: {
 			min: 0,
 			max: 200,
 		},
+		annotations: { points: [] },
 	};
 
 	let chart = new ApexCharts(document.querySelector("#chart"), options);
@@ -308,6 +300,7 @@ function playGame(username, company) {
 	// GROWING CHART
 
 	function addNextDatapoint() {
+		currentDate = availableDates[count];
 		let change =
 			Math.random() > 0.5
 				? Math.floor(Math.random() * 10) + 1
@@ -350,7 +343,7 @@ function playGame(username, company) {
 
 	function handleOrder() {
 		let tradeLi = document.createElement("li");
-		tradeLi.innerText = `${orderButton.innerText} @ ${currentPrice}`;
+		tradeLi.innerText = `${currentDate}: ${orderButton.innerText} @ ${currentPrice}`;
 		tradesLog.append(tradeLi);
 
 		orderButton.innerText == "BUY"
